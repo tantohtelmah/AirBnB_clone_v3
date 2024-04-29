3ddcd3e733b1e397728b7158b4628264ca34c12a
tantohtelmah <tantohtelmah@gmail.com>
2024-04-28 22:24:53 -0400

    task3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Defines routes"""
 
 from flask import jsonify
 from api.v1.views import app_views
@@ -8,10 +9,14 @@ import json
 
 @app_views.route("/status", methods=["GET"])
 def status():
+    """ Route to status"""
+
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", methods=["GET"])
 def stats():
+    """ Route to count objects"""
+
     storage.all()
     return (json.dumps(storage.statInfo))
