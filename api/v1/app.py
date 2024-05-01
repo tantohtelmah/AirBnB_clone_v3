cefc7370e29b96e8eaa9871651c145a11c5440d4
Tundroid <felixkimbu@outlook.com>
2024-05-02 00:20:53 +0100

    Task 9: User Endpoints

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,50 @@
 #!/usr/bin/python3
-""" Endpoint returns the status of the API"""
-
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
-import os
-
-from flask import Flask, jsonify, make_response
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
-# app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True # NOT WORKING
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-# registering the blueprint
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-# defining the teardown_appcontext function
 @app.teardown_appcontext
-def close_storage(exception=None):
-    """Closes the storage connection when the app context is torn down."""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({"error": "Not found"}), 404)
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("HBNB_API_HOST", "0.0.0.0"),
-        port=int(os.environ.get("HBNB_API_PORT", 5000)),
-        threaded=True
-    )
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
