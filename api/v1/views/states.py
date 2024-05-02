b1501b0f378b53f1b6eefdaeef276825ad2ec10e
Tundroid <felixkimbu@outlook.com>
2024-05-02 02:15:21 +0100

    Task 6: State Endpoints

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -39,35 +39,25 @@ def create_state():
     data = request.get_json(silent=True)
     if data is None:
         abort(400, "Not a JSON")
-    if 'name' not in request_data:
+    if 'name' not in data:
         abort(400, "Missing name")
-
-    # create a new state object
-    state = State(**request_data)
-
-    # save new state to database
+    state = State(**data)
     state.save()
 
-    return make_response(jsonify(state.to_dict()), 201)
+    return state.to_dict(), 201
 
 
 @app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """method to update state by id"""
-    # prevent reqeust.get_json from raising exception if
-    # its unable to convert request body to json and rather
-    # return None
-    request_data = request.get_json(silent=True)
-
-    if request_data is None:
+    data = request.get_json(silent=True)
+    if data is None:
         abort(400, "Not a JSON")
-    # search for the state to update based on id
-    for state in storage.all(State).values():
-        if state.id == state_id:
-            for attrib, value in request_data.items():
-                if attrib in ["id", "created_at", "updated_at"]:
-                    continue
-                setattr(state, attrib, value)
-            state.save()
-            return make_response(jsonify(state.to_dict()), 200)
+    state = storage.get(State, state_id)
+    if state:
+        for key, val in data.items():
+            if key not in ["id", "created_at", "updated_at"]:
+                setattr(state, key, val)
+        state.save()
+        return state.to_dict(), 200
     abort(404)
