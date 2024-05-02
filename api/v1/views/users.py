3ea46eac1347b3ab943ccbfcb28be33d2f5e8b72
Tundroid <felixkimbu@outlook.com>
2024-05-02 01:25:55 +0100

    Task 9: User Endpoints

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -25,7 +25,7 @@ def users_routes():
 
         for key in ["email", "password"]:
             if data.get(key) is None:
-                return "Missing {}\n".format(key), 400
+                return f"Missing {key}\n", 400
 
         user = User(**data)
         user.save()
